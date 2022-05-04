c2323c0d42fc012d6ce83640f9178b9dc51b623e
HugooSantos <hugogabrieltjj@gmail.com>
2022-05-04 14:59:07 -0300

    defined d global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+    <Head>
+          <title>home |   HugoNews</title>
+    </Head>
+    <h1 >
+      Hello wor
+    </h1>
+    </>
+
+  )
+}
