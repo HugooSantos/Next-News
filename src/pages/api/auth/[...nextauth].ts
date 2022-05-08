d62594ba7cf36209483fba242f8fdd1879329e44
HugooSantos <hugogabrieltjj@gmail.com>
2022-05-08 19:12:38 -0300

    configured faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q} from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../Services/fauna'
 
 export default NextAuth({
 
@@ -9,6 +11,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user',
     }),
-  
   ],
+  callbacks: {
+    async signIn({ user, account, profile,credentials }) {  
+      const { email } = user
+     try {
+      await fauna.query(
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+          q.Create(
+            q.Collection('users'),
+            {data: { email }}
+          ),
+          q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(user.email)
+            )
+          )
+        )
+      )
+      return true
+     }
+     catch{
+       return false
+     }
+      
+    },
+  }
 })
