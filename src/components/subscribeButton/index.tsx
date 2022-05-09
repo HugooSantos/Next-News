8b99c8b57bc74fac9ec28d11598d57473908a9dc
HugooSantos <hugogabrieltjj@gmail.com>
2022-05-08 21:46:15 -0300

    started to do subscribe button

--- src/components/subscribeButton/index.tsx
+++ src/components/subscribeButton/index.tsx
@@ -1,13 +1,25 @@
+import { useSession, signIn } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
     priceId: string;
 }
 export function SubscribeButton({ priceId}: SubscribeButtonProps){
+    const {data:session, status} = useSession()
+
+    function handleSubscribe(){
+       if(status != "authenticated"){
+        signIn('github')
+        return;
+       }    
+
+
+    }
     return (
         <button
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             subscribe now
         </button>
